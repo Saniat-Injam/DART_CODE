90895505c09f9b26c8861f895c4c0a98e037c5a3
SANIAT INJAM <saniat.cse.ewu@gmail.com>
2025-05-18 19:59:15 +0600

    control flow updated

--- Dart_Basics/5_Control Flow.dart
+++ Dart_Basics/5_Control Flow.dart
@@ -1,56 +1,34 @@
 void main() {
-  
-//   // Simple for loop
-//   for (int i = 0; i < 5; i++) {
-    
-//     print("$i : Hello");
-    
-//   }
-  
-  
-  
-//   List<int> nums = [10, 20, 30, 40, 50];
-  
-//   // in keyword 
-//   for (int i in nums) {
-    
-//      print("The number is $i");
-    
-//   }
-  
-  
-//   // if statement in for loop
-//   for (int i in nums) {
-    
-//     if (i > 20) {
-      
-//       print("Number: $i");
-      
-//     } 
-//   }
-  
-//   // if else statement in for loop
-//   for (int i in nums) {
-    
-//     if (i > 20) {
-      
-//       print("Number: $i");
-      
-//     } else {
-      
-//       print("The number is not high enough");
-      
-//     }
-//   }
-  
-  
-  // where method in for loop
-  
-  List<int> numbers = [100, 200, 300, 400, 500];
-  
-  for (int i in numbers.where((n) => n > 200)) {
-    
-    print("The number is $i");
+  // Simple for loop
+  for (int i = 0; i < 5; i++) {
+    print("The current value of i is $i");
   }
-  
+  print("");
+
+  // for-in loop
+  List<int> numbers = [10, 20, 30, 40, 50];
+
+  for (int number in numbers) {
+    print("The number is $number");
+  }
+  print("");
+
+  // for in loop with if else condition
+  for (int number in numbers) {
+    if (number > 30) {
+      print("The Number is : $number");
+    } else {
+      print("The number isn't hight enough, $number");
+    }
+  }
+  print("");
+
+  // for in loop with where filtering method
+  List<int> marks = [22, 28, 60, 75, 89, 99];
+
+  print(marks.where((s) => s > 60));
+
+  for (int mark in marks.where((s) => s > 60)) {
+    print("The mark is $mark");
+  }
 }
