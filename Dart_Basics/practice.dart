9518979bf818799e49a3290f8a0b1fe66967f6e4
saniat-injam-simanto <136866356+Saniat-Injam@users.noreply.github.com>
2025-05-31 10:53:37 +0600

    async function

--- Dart_Basics/practice.dart
+++ Dart_Basics/practice.dart
@@ -1,1 +1,43 @@
+void main() {
+  //  void getData() {
+  //     Future.delayed(Duration(seconds: 3), () {
+  //       print("SANIAT");
+  //     });
+  //     Future.delayed(Duration(seconds: 2), () {
+  //       print("-Flutter Developer");
+  //     });
+  //    print("All done");
+  //    print("End of the program");
+  //   }
+  //  }
+
+  //   getData();
+
+  //    void getData() async {
+  //     await Future.delayed(Duration(seconds: 3), () {
+  //       print("SANIAT");
+  //     });
+  //     await Future.delayed(Duration(seconds: 2), () {
+  //       print("-Flutter Developer");
+  //     });
+  //    print("All done");
+  //    print("End of the program");
+  //   }
+
+  //   getData();
+
+  void getData() async {
+    String userName = await Future.delayed(Duration(seconds: 3), () {
+      return "SANIAT";
+    });
+    String designation = await Future.delayed(Duration(seconds: 2), () {
+      return "Flutter Developer";
+    });
+    print("$userName \n-$designation");
+    print("All done");
+    print("End of the program");
+  }
+
+  getData();
+}
 
